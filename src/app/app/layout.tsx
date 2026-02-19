350e6a884ea0435494ec933fdfec0d9a1723c217
beyondfocustudios-sudo <beyondfocustudios@gmail.com>
2026-02-19 14:10:19 +0000

    Replace createServerSupabaseClient with createClient

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,4 +1,4 @@
-import { createServerSupabaseClient } from "@/lib/supabase-server";
+import { createClient } from "@/lib/supabase-server";
 import { redirect } from "next/navigation";
 import { AppShell } from "@/components/AppShell";
 
